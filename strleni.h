f029bdfa89235d2e6ff55eea12c3fece3b694db7
Jon Daniel <jondaniel879@gmail.com>
2022-08-26 20:30:36 +0200

    include ilogxi

--- strleni.h
+++ strleni.h
@@ -1,3 +1,29 @@
 #pragma once
 
-int strleni(int i, int base);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ilogxi/ilogxi.h"
+#include <assert.h>
+
+inline intmax_t clamp(intmax_t val, intmax_t lo, intmax_t hi)
+{
+      assert(!(hi < lo));
+
+      return (((val < lo) ? lo : hi) < val) ? hi : val;
+}
+
+inline uint8_t strlenid(int32_t i, uint32_t base)
+{
+	return ilogxid(i, base) + 1;
+}
+
+inline uint8_t strleniq(int64_t i, uint64_t base)
+{
+	return ilogxiq(i, base) + 1;
+}
+
+#ifdef __cplusplus
+}
+#endif
